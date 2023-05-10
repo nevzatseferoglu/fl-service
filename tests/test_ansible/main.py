f14af7b523b0cdb6eee96846db9a9cc72fb698ce
Nevzat Seferoglu <nevzatseferoglu@gmail.com>
2023-05-10 08:01:38 +0300

    bug fix, set up docker install

--- tests/test_ansible/main.py
+++ tests/test_ansible/main.py
@@ -2,23 +2,30 @@ import os
 
 import ansible_runner
 
-DEFAULT_VERBOSITY = 3
+DEFAULT_VERBOSITY = 4
 INVENTORY_DIRECTORY = f"{os.path.dirname(__file__)}/inventory.yaml"
 PLAYBOOK_DIRECTORY = f"{os.path.dirname(__file__)}/playbook.yaml"
 
 
-async def event_handler(event_data: dict):
-    print(event_data["event"])
+# def generator(extravar):
+#     def event_handler(data: dict):
+#         if "event_data" in data:
+#             print("My extravar: ", extravar)
+
+#     return event_handler
 
 
 if __name__ == "__main__":
+    my_object = {"key1": "value1", "key2": "value2", "key3": "value3"}
+
     runner_config = {
         "private_data_dir": "./tmp/ansible-runner/",
         "inventory": INVENTORY_DIRECTORY,
         "playbook": PLAYBOOK_DIRECTORY,
         "verbosity": DEFAULT_VERBOSITY,
-        "event_handler": event_handler,
     }
-    thread, runner = ansible_runner.run_async(**runner_config)
-    thread.join()
+
+    my_object = {"key1": "value4", "key2": "value4", "key3": "value4"}
+
+    runner = ansible_runner.run(**runner_config)
     print(runner.status)
