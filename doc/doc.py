cf54e1c19495c0a09bf9497c9b26ea3bf190a74f
Nevzat Seferoglu <nevzatseferoglu@gmail.com>
2023-05-18 13:54:40 +0300

    fix for netfily

--- doc/doc.py
+++ doc/doc.py
@@ -36,5 +36,5 @@ HTML_TEMPLATE = """<!DOCTYPE html>
 """
 
 if __name__ == "__main__":
-    with open(f"doc.html", "w") as fd:
+    with open(f"index.html", "w") as fd:
         print(HTML_TEMPLATE % json.dumps(app.openapi()), file=fd)
